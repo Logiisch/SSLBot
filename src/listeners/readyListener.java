fc376f76f1786d4d4c4e8c8e7ac048428769cfdb
Logii <ssl.logii@gmail.com>
2023-05-07 21:24:16 +0200

    sorted command registration at readyListener

--- src/listeners/readyListener.java
+++ src/listeners/readyListener.java
@@ -5,18 +5,24 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.jetbrains.annotations.NotNull;
 import util.STATIC;
 import util.SteamConnector;
 
 public class readyListener extends ListenerAdapter {
 
     @Override
-    public void onReady(ReadyEvent event) {
-        registerCommands(event.getJDA());
+    public void onReady(@NotNull ReadyEvent event) {
+        //registerLocalCommands(event.getJDA());
+        //registerGlobalCommands(event.getJDA());
         SteamConnector.loadLinks();
     }
 
-    private void registerCommands(JDA jda) {
+    private static void registerGlobalCommands(JDA jda) {
+
+    }
+
+    private void registerLocalCommands(JDA jda) {
         Guild guild =jda.getGuildById(STATIC.TESTSERVER_ID);
         if (guild ==null) {
             System.out.println("Fehler beim Registieren der Commands: Guild == null");
